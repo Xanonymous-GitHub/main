96a03a48bafdac444e0046e50a61be114a817a43
Xanonymous <trusaidlin@gmail.com>
2019-09-21 10:31:19 +0800

    update the comment

--- hw/hw_chap04_108820003/chap04_project02/chap04_project02.c
+++ hw/hw_chap04_108820003/chap04_project02/chap04_project02.c
@@ -2,7 +2,7 @@
 /* Class: Computer Programming, Fall 2019                        */
 /* Author: 林天佑                                                 */
 /* ID: 108820003                                                 */
-/* Date: 2019.09.17                                              */
+/* Date: 2019.09.19                                              */
 /* Purpose: Be the best student.                                 */
 /* GitHub:  https://github.com/Xanonymous-GitHub/main/tree/HW    */
 /*****************************************************************/
