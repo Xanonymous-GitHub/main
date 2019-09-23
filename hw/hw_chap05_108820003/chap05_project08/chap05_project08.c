732e7c749f48427a4f1db86891932d0ba8fc4605
Xanonymous <trusaidlin@gmail.com>
2019-09-23 15:03:18 +0800

    ch05-8 init

--- hw/hw_chap05_108820003/chap05_project08/chap05_project08.c
+++ hw/hw_chap05_108820003/chap05_project08/chap05_project08.c
@@ -7,7 +7,13 @@
 /* GitHub:  https://github.com/Xanonymous-GitHub/main/tree/HW    */
 /*****************************************************************/
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
 int main(int argc,char* argv[]){
-    
+    struct tm tmdate ={0};
+    printf("Enter a 24-hour time:　");
+    char data;
+    while(scanf("%c",&data)==1)
+    printf("%c",data);
     return 0;
 }
