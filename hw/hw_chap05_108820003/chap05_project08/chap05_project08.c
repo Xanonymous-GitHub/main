c6df1f53bb233e509fb24f7fb0144f05571660f0
Xanonymous <trusaidlin@gmail.com>
2019-09-27 00:34:15 +0800

    struct test

--- hw/hw_chap05_108820003/chap05_project08/chap05_project08.c
+++ hw/hw_chap05_108820003/chap05_project08/chap05_project08.c
@@ -9,7 +9,28 @@
 #include<stdio.h>
 int main(int argc,char* argv[]){
     printf("Enter a 24-hour time:　");
-    int m,s;
-    scanf("%d:%d",&m,&s);
+    int h,m,d=0;
+    scanf("%d:%d",&h,&m);
+    const int dt[]={
+        8*60+0,9*60+43,11*60+19,12*60+47,14*60+0,15*60+45,19*60+0,21*60+45
+    };
+    const char at[]={
+        "10:16 a.m.",
+        "11:52 a.m.",
+        "1:31 p.m.",
+        "3:00 p.m.",
+        "4:08 p.m.",
+        "5:55 p.m.",
+        "9:20 p.m.",
+        "11:58 p.m.",
+        "a.m.","p.m."
+    };
+    m+=60*h;
+    struct timelist
+    {
+        
+    };
+    
+    printf("Closet departure time is %d:%02d %c, arriving at %c\n");
     return 0;
 }
