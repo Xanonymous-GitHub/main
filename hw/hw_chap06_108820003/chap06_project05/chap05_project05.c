a79707e0cb8f60ff7744e5bdb499cbdf1df93836
Xanonymous <trusaidlin@gmail.com>
2019-09-28 18:12:33 +0800

    init ch6

---
+++ hw/hw_chap06_108820003/chap06_project05/chap05_project05.c
@@ -0,0 +1,13 @@
+/*****************************************************************/
+/* Class: Computer Programming, Fall 2019                        */
+/* Author: 林天佑                                                 */
+/* ID: 108820003                                                 */
+/* Date: 2019.                                              */
+/* Purpose: Be the best student.                                 */
+/* GitHub:  https://github.com/Xanonymous-GitHub/main/tree/HW    */
+/*****************************************************************/
+#include<stdio.h>
+int main(int argc,char* argv[]){
+    
+    return 0;
+}
