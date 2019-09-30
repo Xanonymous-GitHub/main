96ac01559bb905f8ea0a6075e94dccd951ba446a
henry3556108 <36980686+henry3556108@users.noreply.github.com>
2019-10-01 01:02:04 +0800

    Recommend you a good packag

--- python/190928.py
+++ python/190928.py
@@ -1,5 +1,8 @@
 import webbrowser as web
 import requests as rq
+import bs4
 url='https://github.com'
 html=rq.get(url)
-print(type(url))
+soup = bs4.BeautifulSoup(html.text)
+print(soup.prettify())
+# print(type(url))
