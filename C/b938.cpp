5ff94ac06cb3bfa9248f51b7c5553699fb10f7af
Xanonymous <trusaidlin@gmail.com>
2019-10-03 21:10:50 +0800

    完成偉大的23行

--- C/b938.cpp
+++ C/b938.cpp
@@ -1,46 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+int man[2000000];
 int main(int argc,char* argv[]){
-    ios::sync_with_stdio(0), cin.tie(0);
-    long m,n;
-
-    //get user data
+    ios::sync_with_stdio(0),cin.tie(0);
+    long m,n,tmp=0;
     cin>>n>>m;
-    long kill[m];
-    for(long i=0;i<m;i++)cin>>kill[i];
-
-    //init a array from 1 to n
-    long man[n];
-    for(long i=0;i<n;i++)man[i]=i+1;
-
-    //kill people
-    for (long i = 0; i < sizeof(kill)/4; i++)
-    {
-        //kill[i] is the current person to be killed now.
-
-        //find if kill[i] is still alive or second last.
-        long pos=0;
-        bool in=false;
-        while (pos<sizeof(man)/4)
-        {
-            if(kill[i]==man[pos]){
-                in=!in;
-                break;
-            }
-            pos++;
+    while (m--){
+        cin>>tmp;
+        if(man[tmp-1]){
+            printf("0u0 ...... ?\n");
+            continue;
         }
-        if (in&&(pos<sizeof(man)/4-1))
-        {
-            cout<<man[pos+1]<<endl;
-            man.erase(man.begin()+pos+1);
+        while (man[tmp]&&tmp<n)tmp++;
+        if(tmp!=n){
+            man[tmp]=!man[tmp];
+            printf("%ld\n",tmp+1);
         }
-        else{
-            cout<<"0u0 ...... ?"<<endl;
-        }
-        
-        
-        //if true,remove its next element and print it.
-        //else,print "0u0 ...... ?" 
+        else
+        printf("0u0 ...... ?\n");
     }
     return 0;
 }
