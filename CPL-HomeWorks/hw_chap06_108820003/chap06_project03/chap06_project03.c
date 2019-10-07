58623e69ea9e64ca655a1869b8dcb1d70153a893
Xanonymous <trusaidlin@gmail.com>
2019-10-07 15:20:14 +0800

    ch06-3

--- CPL-HomeWorks/hw_chap06_108820003/chap06_project03/chap06_project03.c
+++ CPL-HomeWorks/hw_chap06_108820003/chap06_project03/chap06_project03.c
@@ -7,7 +7,22 @@
 /* GitHub:  https://github.com/Xanonymous-GitHub/main/tree/HW    */
 /*****************************************************************/
 #include<stdio.h>
+int GCD(int a,int b){
+    //Euclidean algorithm (a type of recursion)
+    if(a==0)return b;
+    if(b==0)return a;
+    return GCD(b,(a-b*(a/b)));
+}
 int main(int argc,char* argv[]){
-    
+    int c1=1,c2=1,gcd=0;
+    printf("Enter a fraction: ");
+    scanf("%d/%d",&c1,&c2);
+    gcd=GCD((c2>c1)?c2:c1,(c2<c1)?c2:c1);
+    printf("In lowest terms: ");
+    if(c2==gcd){
+        printf("%d\n",c1/gcd);
+    }else{
+        printf("%d/%d\n",c1/gcd,c2/gcd);
+    }
     return 0;
 }
