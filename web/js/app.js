3891e1ebc1bf0bfaa419dca4f5b0e380ad4fc1fe
Xanonymous <trusaidlin@gmail.com>
2019-10-07 23:06:30 +0800

    go,java,vue

--- web/js/app.js
+++ web/js/app.js
@@ -2,6 +2,17 @@ var app= new Vue({
     el: '#app',
     data: {
       message: 'Hello World!',
-      lsdfjksld:'dfdifjdif'
+      lsdfjksld:'dfdifjdif',
+      dfk:[
+        {cc:'31321'},
+        {cc:'321321979879'},
+        {tex:'6565165165'},
+        {tex:'wefwefwefwefwefefelweflkenlfknkn'}
+      ]
+    },
+    method:{
+      onclick(){
+        
+      }
     }
 });
