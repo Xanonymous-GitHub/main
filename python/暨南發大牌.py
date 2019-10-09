9e43260247e33625312d150e5e203c469ff79005
Xanonymous <trusaidlin@gmail.com>
2019-10-10 00:15:27 +0800

    fixed

--- python/暨南發大牌.py
+++ python/暨南發大牌.py
@@ -2,26 +2,23 @@ import random as R
 def output(i,S,H,D,C):
     print('S ',end='')
     for x in range(13):
-        if S[x][1]==i:
-            print(S[x][0],end='',sep='')
+        if S[x][1]==i:print(S[x][0],end='',sep='')
     print()
     print('H ',end='')
     for x in range(13):
-        if H[x][1]==i:
-            print(S[x][0],end='',sep='')
+        if H[x][1]==i:print(H[x][0],end='',sep='')
     print()
     print('D ',end='')
     for x in range(13):
-        if D[x][1]==i:
-            print(S[x][0],end='',sep='')
+        if D[x][1]==i:print(D[x][0],end='',sep='')
     print()
     print('C ',end='')
     for x in range(13):
-        if C[x][1]==i:
-            print(S[x][0],end='',sep='')
+        if C[x][1]==i:print(C[x][0],end='',sep='')
     print()
 def main():
     card=list('A23456789TJQK')
+    area=['North:','East:','South:','West:']
     S=[[]for x in range(13)]
     H=[[]for x in range(13)]
     D=[[]for x in range(13)]
@@ -35,13 +32,7 @@ def main():
         D[i].append(R.randint(1,4))
         C[i].append(card[i])
         C[i].append(R.randint(1,4))
-
-    print('North:')
-    output(1,S,H,D,C)
-    print('East:')
-    output(2,S,H,D,C)
-    print('South:')
-    output(3,S,H,D,C)
-    print('West:')
-    output(4,S,H,D,C)
+    for x in range(4):
+        print(area[x])
+        output(x+1,S,H,D,C)
 if __name__=='__main__':main()
