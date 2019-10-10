0d18c2ea4c1bfbc5e8f39b1b1c72335100ba27ea
Xanonymous <trusaidlin@gmail.com>
2019-10-10 22:55:12 +0800

    ToDolist init first version

---
+++ C/quicksort.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int quicksort(vector<int> data){
+    
+    return 0;
+}
+int main(int argc,char* argv[]){
+    ios::sync_with_stdio(0),cin.tie(0);
+    //get user data
+    int n=0;
+    cin>>n;
+    vector<int> data(n);
+    for (int i = 0; i < n; i++)cin>>data[i];
+    quicksort(data);
+    return 0;
+}
---
+++ python/quicksort.py
@@ -0,0 +1,5 @@
+def quicksort(data):
+
+    return 0
+data=list(map(int,input().split()))
+quicksort(data)
