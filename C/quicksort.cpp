613784f91ee65fdd5d6f1a2b1cc8d2a309af3543
Xanonymous <trusaidlin@gmail.com>
2019-10-12 00:08:37 +0800

    qs cpp init

--- C/quicksort.cpp
+++ C/quicksort.cpp
@@ -1,1 +1,14 @@
-//
+#include<bits/stdc++.h>
+using namespace std;
+int qs(int* data){
+    
+}
+int main(int argc,char* argv[]){
+    ios::sync_with_stdio(0),cin.tie(0);
+    int n=0;
+    cin>>n;
+    vector<int> data(n);
+    for (int i = 0; i < n; i++)cin>>data[i];
+    
+    return 0;
+}
