1442893327ba1feffd470bedb92235ed8919de98
Xanonymous <trusaidlin@gmail.com>
2019-10-11 23:39:06 +0800

    快速排序法 python 正式釋出

--- python/quicksort.py
+++ python/quicksort.py
@@ -1,10 +1,11 @@
 def qs(data):
-    if len(data)>1:
-        big,small,center=list(),list(),list()
-        for x in data:
-            if x>data[0]:big.append(x)
-            if x==data[0]:center.append(x)
-            if x<data[0]:small.append(x)
-        return qs(small)+center+qs(big)
-    else:return data
+    if data==[]:return []
+    SFR=len(data)-1
+    BFL=0
+    while(BFL!=SFR):
+        while(SFR>0 and data[SFR]>=data[0]):SFR-=1
+        while(BFL!=SFR and BFL<len(data)-1 and data[BFL]<=data[0]):BFL+=1
+        if(BFL!=SFR):data[SFR],data[BFL]=data[BFL],data[SFR]
+    data[0],data[BFL]=data[BFL],data[0]
+    return qs(data[:BFL])+[data[BFL]]+qs(data[BFL+1:])
 print(qs(list(map(int,input().split()))))
