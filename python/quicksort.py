0800caeafdbe490464ff3838cbc86fd497a7d817
Xanonymous <trusaidlin@gmail.com>
2019-10-11 16:52:11 +0800

    build the quicksort (not finished yet.)

--- python/quicksort.py
+++ python/quicksort.py
@@ -1,5 +1,18 @@
-def quicksort(data):
-
-    return 0
+def qs(data):
+    #find the low & high
+    low,high=1,len(data)-1
+    #low=>from left,the first num which smaller than data[0]
+    while (data[low]>=data[0])and(low<len(data)-1):low+=1
+    #high=>from right,the first num which bigger than data[0]
+    while (data[high]<=data[0])and(high>0):high-=1
+    if low>high:data[low],data[high]=data[high],data[low]
+    else:
+        data[0],data[low]=data[low],data[0]
+        #left branch
+        qs(data[:low])
+        #right branch
+        qs(data[low:])
+    return
 data=list(map(int,input().split()))
-quicksort(data)
+qs(data)
+print()
