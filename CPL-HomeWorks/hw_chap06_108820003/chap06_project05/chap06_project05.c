eb977a6369f17cc2887938cef38670d3667050a8
Xanonymous <trusaidlin@gmail.com>
2019-10-12 01:20:30 +0800

    finish ch6

--- CPL-HomeWorks/hw_chap06_108820003/chap06_project05/chap06_project05.c
+++ CPL-HomeWorks/hw_chap06_108820003/chap06_project05/chap06_project05.c
@@ -2,12 +2,24 @@
 /* Class: Computer Programming, Fall 2019                        */
 /* Author: 林天佑                                                 */
 /* ID: 108820003                                                 */
-/* Date: 2019.                                              */
+/* Date: 2019.10.12                                              */
 /* Purpose: Be the best student.                                 */
 /* GitHub:  https://github.com/Xanonymous-GitHub/main/tree/HW    */
 /*****************************************************************/
 #include<stdio.h>
 int main(int argc,char* argv[]){
-    
+    printf("Enter a number: ");//提示輸入訊息
+    int data=0;//除存輸入資料
+    scanf("%d",&data);//接收輸入
+    printf("The reversal is: ");//輸出提示訊息
+    do{
+        /*
+            原資料除以10的餘數就是該數的最末位數，依序輸出到CLI，將逆向重組。
+            取得餘數後，將原資料更改為本身除以10的商，即可達到有如刪除末位之效果。
+            當原數字最高位變成新數字最末位輸出後，原資料再次更改為本身除以10的商，會得到0。
+        */
+        printf("%d",data%10);//輸出原資料除以10的餘數
+        data/=10;//將原資料更改為本身除以10的商
+    }while(data);//若資料尚未變成0則繼續執行操作。
     return 0;
 }
--- CPL-HomeWorks/hw_chap06_108820003/chap06_project11/chap06_project11.c
+++ CPL-HomeWorks/hw_chap06_108820003/chap06_project11/chap06_project11.c
@@ -2,12 +2,30 @@
 /* Class: Computer Programming, Fall 2019                        */
 /* Author: 林天佑                                                 */
 /* ID: 108820003                                                 */
-/* Date: 2019.                                              */
+/* Date: 2019.10.12                                              */
 /* Purpose: Be the best student.                                 */
 /* GitHub:  https://github.com/Xanonymous-GitHub/main/tree/HW    */
 /*****************************************************************/
 #include<stdio.h>
+double ngia(int n){
+    //計算某數的n階乘
+    long result=1;//定義一將要被乘的變數
+    for (int i = 1; i < n+1; i++)result*=i;//開始乘，每次乘數+1
+    return result;//返回答案
+}
 int main(int argc,char* argv[]){
-    
+    printf("Enter a number: ");//提示輸入訊息
+    int n=0;//除存輸入資料
+    double e=1;//定義除存結果的變數，並已經+1
+    scanf("%d",&n);//接收輸入
+    for (int i = n; i > 0; i--){
+        e+=1/ngia(i);//每次加入i階分之一
+    }
+    printf("The e value of input is %.50lf\n",e);
+    /*  
+        limit(x->無窮):1+1/1!+1/2!+.....1/x!近似於以下數字
+        2.71828182845904553488480814849026501178741455078125 
+        該數字就是題目的極限答案(double的極限位數)，因此預設輸出50位數的小數點。
+    */
     return 0;
 }
