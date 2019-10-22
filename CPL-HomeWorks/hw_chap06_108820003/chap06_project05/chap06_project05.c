4be9164f613466006e3e10a57d40e43f070de52c
TeU-Lin <trusaidlin@gmail.com>
2019-10-22 16:32:11 +0800

    Update chap06_project05.c

--- CPL-HomeWorks/hw_chap06_108820003/chap06_project05/chap06_project05.c
+++ CPL-HomeWorks/hw_chap06_108820003/chap06_project05/chap06_project05.c
@@ -1,4 +1,4 @@
-/*****************************************************************/
+//*****************************************************************/
 /* Class: Computer Programming, Fall 2019                        */
 /* Author: 林天佑                                                 */
 /* ID: 108820003                                                 */
@@ -6,20 +6,15 @@
 /* Purpose: Be the best student.                                 */
 /* GitHub:  https://github.com/Xanonymous-GitHub/main/tree/HW    */
 /*****************************************************************/
-#include<stdio.h>
-int main(int argc,char* argv[]){
-    printf("Enter a integer : ");//提示輸入訊息
-    long long data=0;//除存輸入資料
-    scanf("%lld",&data);//接收輸入
-    printf("Digits reversed : ");//輸出提示訊息
-    do{
-        /*
-            原資料除以10的餘數就是該數的最末位數，依序輸出到CLI(Command Line Interface)，將逆向重組。
-            取得餘數後，將原資料更改為本身除以10的商，即可達到有如刪除末位之效果。
-            當原數字最高位變成新數字最末位輸出後，原資料再次更改為本身除以10的商，會得到0。
-        */
-        printf("%lld",data%10);//輸出原資料除以10的餘數
-        data/=10;//將原資料更改為本身除以10的商
-    }while(data);//若資料尚未變成0則繼續執行操作。
+#include <stdio.h>
+#define len 1000
+char data[len];
+int main(int argc,char* argv[]) {
+		printf("Enter a integer : ");//提示輸入訊息
+		scanf("%s",data);//讀取輸入
+		printf("Digits reversed : \n");//輸出提示訊息
+		for(int i=len-1;i>=0;i--)//逆向迴圈
+        printf("%c",data[i]);//逆向輸出
+		printf("\n");//換行
     return 0;
 }
