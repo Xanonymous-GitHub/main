fa8b16f31e2292b6866489603c38ca2834ac91d0
Xanonymous <trusaidlin@gmail.com>
2019-11-07 21:24:12 +0800

    aaaaaa

--- CPL-HomeWorks/hw_chap09_108820003/chap09_project07/chap09_project07.c
+++ CPL-HomeWorks/hw_chap09_108820003/chap09_project07/chap09_project07.c
@@ -2,13 +2,21 @@
 /* Class: Computer Programming, Fall 2019                        */
 /* Author: 林天佑                                                 */
 /* ID: 108820003                                                 */
-/* Date: 2019.                                              */
+/* Date: 2019.11.07                                              */
 /* Purpose: Be the best student.                                 */
 /* GitHub:  https://github.com/Xanonymous-GitHub/main/tree/HW    */
 /*****************************************************************/
 #include <stdio.h>
+int power(){
+    
+}
 int main(int argc, char *argv[])
 {
-
+    int x,n;
+    printf("For x^n, enter x:");
+    scanf("%d",&x);
+    printf("Enter n:");
+    scanf("%d", &n);
+    printf("");
     return 0;
 }
