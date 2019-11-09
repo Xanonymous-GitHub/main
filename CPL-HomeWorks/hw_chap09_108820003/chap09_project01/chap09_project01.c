b9ff306d0c7f6540531e3e45508a05b9b0c45cca
Xanonymous <trusaidlin@gmail.com>
2019-11-09 12:15:34 +0800

    ch9-1 fixed

--- CPL-HomeWorks/hw_chap09_108820003/chap09_project01/chap09_project01.c
+++ CPL-HomeWorks/hw_chap09_108820003/chap09_project01/chap09_project01.c
@@ -18,14 +18,14 @@ int *selection_sort(int *data, int size)
         count:當前判斷的位置
     */
     int base, min, count;
-    for (base = 0; base < size - 1; base++)//判斷到倒數第一位
+    for (base = 0; base < size - 1; base++) //判斷到倒數第一位
     {
-        min = base;//預設最小值是在基底
-        for (count = base + 1; count < size; count++)//開始判斷基底後的所有項
+        min = base;                                   //預設最小值是在基底
+        for (count = base + 1; count < size; count++) //開始判斷基底後的所有項
         {
-            if (*(data + count) < *(data + min))//如果這項小於基底
+            if (*(data + count) < *(data + min)) //如果這項小於基底
             {
-                min = count;//將最小項位置設定到這個位置
+                min = count; //將最小項位置設定到這個位置
             }
         }
         /*
@@ -35,28 +35,28 @@ int *selection_sort(int *data, int size)
         *(data + min) = *(data + base);
         *(data + base) = tmp;
     }
-    return data;//傳回記憶體位址
+    return data; //傳回記憶體位址
 }
 int main(int argc, char *argv[])
 {
-    printf("Enter list of integers to be sorted: ");//顯示輸入訊息
+    printf("Enter list of integers to be sorted: "); //顯示輸入訊息
     /*
         size:輸入的資筆數
         data:儲存輸入的數字們
     */
     int size = 0, data[MAX_INPUT_LENGTH];
-    while (size < MAX_INPUT_LENGTH)//限制不要讀取超過筆數限制
+    char c;
+    while ((size < MAX_INPUT_LENGTH) && ((c = getchar()) != '\n')) //限制不要讀取超過筆數限制且非最後項
     {
-        scanf("%d", &data[size++]);//從stdin讀取一個數字
-        if (getchar() == '\n')//如果這個數字後面是換行符號
-            break;//就停止讀取
+        ungetc(c, stdin);           //退回預判字元到標準輸入流
+        scanf("%d", &data[size++]); //從stdin讀取一個數字
     }
-    int *result = selection_sort(&data[0], size);//建立指標，指向選擇排序後的回傳位址
-    printf("Sorted list: ");//顯示輸出訊息
-    for (int i = 0; i < size - 1; i++)//將結果輸出但保留最後位
+    int *result = selection_sort(&data[0], size); //建立指標，指向選擇排序後的回傳位址
+    printf("Sorted list: ");                      //顯示輸出訊息
+    for (int i = 0; i < size - 1; i++)            //將結果輸出但保留最後位
     {
         printf("%d ", result[i]);
     }
-    printf("%d\n", result[size - 1]);//輸出最後位並換行，避免多餘空格
+    printf("%d\n", result[size - 1]); //輸出最後位並換行，避免多餘空格
     return 0;
 }
