ba749410c0c0133f5fd03f5e99d63f8f04574536
Xanonymous <trusaidlin@gmail.com>
2019-12-06 20:21:48 +0800

    公佈答案

--- python/191206.py
+++ python/191206.py
@@ -1,2 +1,9 @@
 import random as R
-print(''.join(map(str,R.sample([chr(x) for x in range(32,127)],R.randint(8,12)))))
+print(''.join(R.sample([chr(x) for x in range(32, 127)], R.randint(8, 12))))
+
+字典=list()
+for x in range(32, 127):
+    字典.append(chr(x))
+幾個字 = R.randint(8, 12)
+挑出的字 = R.sample(字典, 幾個字)
+print(''.join(挑出的字))
