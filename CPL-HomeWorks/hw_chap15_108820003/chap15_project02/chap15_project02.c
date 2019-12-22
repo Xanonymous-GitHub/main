59d6c92af74623480f177725b66e5cb873c559d3
Xanonymous <trusaidlin@gmail.com>
2019-12-22 16:42:23 +0800

    15-2 problem

--- CPL-HomeWorks/hw_chap15_108820003/chap15_project02/chap15_project02.c
+++ CPL-HomeWorks/hw_chap15_108820003/chap15_project02/chap15_project02.c
@@ -6,8 +6,28 @@
 /* Purpose: Be the best student.                                 */
 /* GitHub:  https://github.com/Xanonymous-GitHub/main/tree/HW    */
 /*****************************************************************/
-#include <stdio.h>
+#include "line.h"
+#include "word.h"
+#include <string.h>
+#define MAX_WORD_LEN 20
 int main(int argc, char *argv[]) {
-    
+    char word[MAX_WORD_LEN + 2];
+    int word_len;
+    clear_line();
+    for (;;) {
+        read_word(word, MAX_WORD_LEN + 1);
+        word_len = strlen(word);
+        if (word_len == 0) {
+            flush_line();
+            return 0;
+        }
+        if (word_len > MAX_WORD_LEN)
+            word[MAX_WORD_LEN] = '*';
+        if (word_len + 1 > space_remaining()) {
+            write_line();
+            clear_line();
+        }
+        add_word(word);
+    }
     return 0;
 }
